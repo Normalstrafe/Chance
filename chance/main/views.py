441cb329bc86c2dd34ac41d401f0c382cad9d14e
normalstrafe <nazarsavjak@gmail.com>
2020-12-27 16:11:22 +0200

    fix problem

---
+++ chance/main/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import os
+from datetime import date
+
+
+def main(request):
+    return render(request, 'main.html')
+
+
+def health(request):
+    response = {
+        'date': date.today(),
+        'current_page': request.build_absolute_uri(),
+        'server_info': request.META['SERVER_NAME'],
+        'client_info': {
+            'user_agent': request.META['HTTP_USER_AGENT'],
+            'user_ip': request.META['REMOTE_ADDR']
+        }
+    }
+    return JsonResponse(response)
